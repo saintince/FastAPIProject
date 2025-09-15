3dec54bd65b5a8b6500d8537a4af9bdba52127fe
EVOSandru6 <EVOSduple@mail.ru>
2025-09-15 18:00:09 +0300

    дб2

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI, Depends, HTTPException
+from datetime import timedelta, datetime
+from fastapi import FastAPI, Depends, HTTPException, status
+import jwt
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, Session, sessionmaker
@@ -8,11 +12,52 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+SECRET_KEY = "my_secret"
+ALGORITHM = "HS256"
+
+pwd_context =CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def verify_password(plain_pass,  hashed_pass):
+    return pwd_context.verify(plain_pass, hashed_pass)
+
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=15)):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Could not validate credentials",headers={"WWW-Authenticate":"Bearer"})
+        return username
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401,detail="Token expired")
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401,detail="Invalid token")
+
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique= True, index=True)
+    hashed_password = Column(String)
+
 
 class Task(Base):
     __tablename__ = "tasks"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
+    owner = Column(String, index=True)
+
 
 
 Base.metadata.create_all(bind=engine)
@@ -24,10 +69,11 @@ class TaskCreate(BaseModel):
 
 class TaskResponse(BaseModel):
     id: int
-    tite: str
+    title: str
+    owner: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 app = FastAPI()
@@ -40,10 +86,29 @@ def get_db():
     finally:
         db.close()
 
+@app.post("register")
+def register(username: str, password: str, db: Session = Depends(get_db)):
+    if db.query(User).filter(User.username == username).first():
+        raise HTTPException(status_code=400, detail="User already exists")
+    user = User(username=username, hashed_password=hash_password(password))
+    db.add(user)
+    db.commit()
+    return {"message": "User registered"}
+
+
+
+@app.post("/login")
+def  login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=400,detail="Incorrect password or username")
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
 
 @app.post("/todos/", response_model=TaskResponse)
-def read_tasks(task: TaskCreate, db: Session = Depends(get_db)):
-    db_task = Task(title=task.title)
+def create_tasks(task: TaskCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    db_task = Task(title=task.title, owner=current_user.username)
     db.add(db_task)
     db.commit()
     db.refresh(db_task)
@@ -51,15 +116,18 @@ def read_tasks(task: TaskCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/todos/", response_model=list[TaskResponse])
-def get_tasks(db: Session = Depends(get_db)):
-    return db.query(Task).all()
+def get_tasks(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return db.query(Task).filter(Task.owner == current_user.username).all()
 
 
 @app.delete("/todos/{todo_id}", response_model=dict)
-def delete_task(task_id: int, db: Session = Depends(get_db)):
-    task = db.query(Task).filter(Task.id == task_id).first()
+def delete_task(task_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    task = db.query(Task).filter(Task.id == task_id, Task.owner == current_user.username).first()
     if not task:
         raise HTTPException(status_code=404, detail="Не найдено")
     db.delete(task)
     db.commit()
     return {"message": f"Задача {task_id} удалена "}
+
+
+
